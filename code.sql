c83f4f51539c1bf343932b466421b196196f321a
Animesh Dutta <34543420+animesh-dutta@users.noreply.github.com>
2023-03-30 22:01:34 +0530

    Update code.sql

--- code.sql
+++ code.sql
@@ -1,12 +1,10 @@
 /*    
-
 Author:Animesh Dutta
 Created On:December 18,2017
 File Description:It is a Sample SQL File.More Files will be added later On in the current Project Folder
 Caution:When you are writing any SQL Script,you should know in which Relational Database Management System(RDBMS),you are
 going to run the SQL Script.There may be slight changes in the SQL Scripts according to the RDBMS you are going to use.
 File Name:code.sql
-
 
 */
 
