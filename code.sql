757b2e81808b64bd80b17fee2c8f6687681d262a
Animesh Dutta <34543420+animesh-dutta@users.noreply.github.com>
2025-02-01 16:52:51 +0530

    Update code.sql

--- code.sql
+++ code.sql
@@ -4,7 +4,7 @@ Created On:December 18,2017
 File Description:It is a Sample SQL File.More Files will be added later On in the current Project Folder
 Caution:When you are writing any SQL Script,you should know in which Relational Database Management System(RDBMS),you are
 going to run the SQL Script.There may be slight changes in the SQL Scripts according to the RDBMS you are going to use.
-File Name:code.sql 
+File Name:code.sql  
 */ 
 Select * from  Users  /* getting all the fields results from Users tables */
 Select count (*) from Users /* Result Count */
